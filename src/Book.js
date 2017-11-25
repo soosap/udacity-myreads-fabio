0bf6ca364bc8631559f0562841c036761432a98c
FabioDeVasco <de.vasconcelos.fabio@gmail.com>
2017-11-25 17:48:01 +0100

    books out

--- src/Book.js
+++ src/Book.js
@@ -1,12 +1,12 @@
 import * as React from 'react';
 
-const Book = () => {
+const Book = ({author, titel, backgroundurl}) => {
   return (
 
     <li>
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${backgroundurl}")`}}></div>
           <div className="book-shelf-changer">
             <select>
               <option value="none" disabled>Move to...</option>
@@ -17,8 +17,8 @@ const Book = () => {
             </select>
           </div>
         </div>
-        <div className="book-title">To Kill a Mockingbird</div>
-        <div className="book-authors">Harper Lee</div>
+        <div className="book-title">{titel}</div>
+        <div className="book-authors">{author}</div>
       </div>
     </li>
 
